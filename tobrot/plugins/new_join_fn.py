9a0a88b35eef570631288b3ea1142f2fe0737682
Balaji Shinnosuke <62749324+bluv-pr@users.noreply.github.com>
2021-05-01 19:28:37 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -25,6 +25,6 @@ async def help_message_f(client, message):
     # display the /help
 
     await message.reply_text(
-        """Hello 👾 !\nThis Is A Telegram Leech Bot 🧲\nClick Below To Know How To Use Me 📄\n Developer 👨🏻‍💻 : @ML_BotUpdates""",
+        """""",
         disable_web_page_preview=True,
     )
