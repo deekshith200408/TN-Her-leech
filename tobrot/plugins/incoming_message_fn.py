116485e17c059c3f58013d0bab5d522fe3cd0166
Jikku <66976170+jikkubot@users.noreply.github.com>
2021-05-06 13:37:56 +0400

    Update incoming_message_fn.py

--- tobrot/plugins/incoming_message_fn.py
+++ tobrot/plugins/incoming_message_fn.py
@@ -266,7 +266,7 @@ async def rename_tg_file(client, message):
                 message_id = final_response[key_f_res_se]
                 channel_id = str(message.chat.id)[4:]
                 private_link = f"https://t.me/c/{channel_id}/{message_id}"
-                message_to_send += "✘ <a href='"
+                message_to_send += "👉 <a href='"
                 message_to_send += private_link
                 message_to_send += "'>"
                 message_to_send += local_file_name
@@ -277,7 +277,7 @@ async def rename_tg_file(client, message):
                     f"<b><a href='tg://user?id={usr_id}'>📂 Your Requested Files</a></b>\n\n"
                 )
                 message_to_send = mention_req_user + message_to_send
-                message_to_send = message_to_send + "\n\n" + "#uploads\n\n<b>💢 Powered By : @TamilMoviesML</b>"
+                message_to_send = message_to_send + "\n\n" + "#uploads\n\n<b>💢 Powered By : @FilmClubGroup</b>"
             else:
                 message_to_send = "<i>FAILED</i> to upload files. 😞😞"
             await message.reply_text(
